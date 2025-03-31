1a1e046cba4d5d3227c7b15a12c1bc884b9f3b3e
Luccelillja <89689903+Luccelillja@users.noreply.github.com>
2025-04-01 00:09:16 +0300

    Update Footer.jsx

--- my-web3-portfolio/src/components/Footer.jsx
+++ my-web3-portfolio/src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
     <motion.footer
       ref={ref}
       className={styles.footer}
-      initial={{ opacity: 0, y: 40 }}
+      // initial={{ opacity: 0, y: 40 }}
       animate={isInView ? { opacity: 1, y: 0 } : {}}
       transition={{ duration: 0.8, delay: 0.4, ease: "easeOut" }}
     >
